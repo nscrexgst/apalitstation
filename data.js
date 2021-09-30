3dd2d22c930c03eec2577a61bffac06bb4472bf2
krquinton <nscrexgst.dotr@gmail.com>
2021-09-30 16:42:05 +0800

    initial commit
    initial commit

---
+++ data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-apalit-station-1",
+      "name": "Apalit Station 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.018098753005604706,
+        "pitch": 0.6025033640955737,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22777122812301087,
+          "pitch": 0.1993501098662378,
+          "rotation": 0,
+          "target": "1-apalit-station-2"
+        },
+        {
+          "yaw": -3.0001996705624165,
+          "pitch": 0.2576001721883667,
+          "rotation": 0,
+          "target": "2-apalit-station-3"
+        },
+        {
+          "yaw": -2.968512446030916,
+          "pitch": 0.06567859138944598,
+          "rotation": 0,
+          "target": "3-site-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-apalit-station-2",
+      "name": "Apalit Station 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.31281670439591913,
+        "pitch": 0.5146342456306208,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-apalit-station-3",
+      "name": "Apalit Station 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-site-office",
+      "name": "Site Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0751834139320664,
+        "pitch": 0.0018379794486804713,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4342022903971028,
+          "pitch": 0.007957725837229646,
+          "rotation": 0,
+          "target": "4-construction-yard"
+        },
+        {
+          "yaw": 2.8577626003395853,
+          "pitch": -0.007301377256506925,
+          "rotation": 0,
+          "target": "0-apalit-station-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-construction-yard",
+      "name": "Construction Yard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.08602604237983869,
+        "pitch": 0.5397592468987966,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0954446316489914,
+          "pitch": 0.25609576013336977,
+          "rotation": 0,
+          "target": "3-site-office"
+        },
+        {
+          "yaw": -3.011200369321447,
+          "pitch": 0.046545231630004125,
+          "rotation": 0,
+          "target": "0-apalit-station-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "09302021 Apalit Station",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
